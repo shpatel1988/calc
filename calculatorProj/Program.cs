90c42de8a0e27efa0c8156deb2b9eb261235ee6a
shrikant h patel <shpatel1988@gmail.com>
2020-07-14 19:34:44 +0530

    this is sum

--- calculatorProj/Program.cs
+++ calculatorProj/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,11 +11,17 @@ namespace calculatorProj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world ");
-            Console.WriteLine("hello world ");
-            Console.WriteLine("hello world ");
+            Console.Write("Enter the numbe 1:");
             string str =Console.ReadLine();
+            double n1 = Convert.ToDouble(str);
             Console.WriteLine($"You have entered {str}");
+
+            Console.Write("Enter the numbe 2:");
+            string str1 = Console.ReadLine();
+            double n2 = Convert.ToDouble(str1);
+            Console.WriteLine($"You have entered {str1}");
+
+            Console.WriteLine($"sum = {n1 + n2}");
             Console.Read();
         }
     }
